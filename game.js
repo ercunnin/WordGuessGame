9016d86e180f811cce2681892338a454abb3f85d
The Dude <thedude@Erics-MacBook-Pro.local>
2019-03-03 17:08:49 -0700

    js

--- game.js
+++ game.js
@@ -1,29 +1,107 @@
 
-var $ = function(id) {
-    return document.getElementById(id);
-}
+$(document).ready(function () {
+
+    var possibleWords = ["Dwight", "Jim", "Michael", "Stanley", "Phylis", "Pam", "Oscar", "Andy", "Kevin", "Angela", "Creed", "Ryan", "Kelly", "Toby", "Meredith"]
+
+    const maxGuess = 10
+    var pauseGame = false
+
+    var guessedLetters = []
+    var guessingWord = []
+    var wordToMatch
+    var numGuess
+    var wins = 0
+
+    resetGame()
 
 
-//my array
-var names = ["Dwight", "Jim", "Michael", "Stanley", "Phylis", "Pam", "Oscar", "Andy", "Kevin", "Angela", "Creed", "Ryan", "Kelly", "Toby", "Meredith"]
-var choice = Math.floor(Math.random()*15);
-var answer = game[choice];
-var myLength = answer.length;
-var display=[myLength];
-var attemptsLeft= 8;
-var output="";
-var userLetter="";
-var setup = function(){
-    for (var i=0; i< answer.length; i++)
-{
-display[i] = "_ ";
-output = output + display[i];
-}
-document.getElementById("names").innerHTML = output;
-output = "";
-}
-var submit = function(){
-    output = "";
-    userLetter=$("letter").value;
-    $("letter").value ="";
-}
+    document.onkeypress = function (event) {
+        if (isAlpha(event.key) && !pauseGame) {
+            checkForLetter(event.key.toUpperCase())
+        }
+    }
+
+
+    function checkForLetter(letter) {
+        var foundLetter = false
+        var correctSound = document.createElement("audio")
+        var incorrectSound = document.createElement("audio")
+        correctSound.setAttribute("src", "assets/sounds/stairs.mp3")
+        incorrectSound.setAttribute("src", "assets/sounds/croak.mp3")
+
+
+        for (var i = 0, j = wordToMatch.length; i < j; i++) {
+            if (letter === wordToMatch[i]) {
+                guessingWord[i] = letter
+                foundLetter = true
+                correctSound.play()
+
+                if (guessingWord.join("") === wordToMatch) {
+
+                    wins++
+                    pauseGame = true
+                    updateDisplay()
+                    setTimeout(resetGame, 5000)
+                }
+            }
+        }
+
+        if (!foundLetter) {
+            incorrectSound.play()
+
+            if (!guessedLetters.includes(letter)) {
+
+                guessedLetters.push(letter)
+
+                numGuess--
+            }
+            if (numGuess === 0) {
+
+                guessingWord = wordToMatch.split()
+                pauseGame = true
+                setTimeout(resetGame, 5000)
+            }
+        }
+
+        updateDisplay()
+
+    }
+
+    function isAlpha(ch) {
+        return /^[A-Z]$/i.test(ch);
+    }
+
+    function resetGame() {
+        numGuess = maxGuess
+        pauseGame = false
+
+
+        wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].toUpperCase()
+        console.log(wordToMatch)
+
+
+        guessedLetters = []
+        guessingWord = []
+
+
+        for (var i = 0, j = wordToMatch.length; i < j; i++) {
+
+            if (wordToMatch[i] === " ") {
+                guessingWord.push(" ")
+            } else {
+                guessingWord.push("_")
+            }
+        }
+
+
+        updateDisplay()
+    }
+
+    function updateDisplay() {
+        document.getElementById("totalWins").innerText = wins
+        document.getElementById("currentWord").innerText = guessingWord.join("")
+        document.getElementById("remainingGuesses").innerText = numGuess
+        document.getElementById("guessedLetters").innerText = guessedLetters.join(" ")
+    }
+});
+
